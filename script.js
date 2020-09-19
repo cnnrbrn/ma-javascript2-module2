1feedc18f03ca92c8b49df3dd189383c0f7d58f8
cnnrbrn <connorbobrien@gmail.com>
2020-09-19 11:03:37 +0200

    Add array

---
+++ script.js
@@ -0,0 +1,18 @@
+const books = [
+    {
+        isbn: "0-2867-2575-0",
+        title: "Great book",
+    },
+    {
+        isbn: "1-9876-9548-4",
+        title: "Ok book",
+    },
+    {
+        isbn: "1-6825-8790-0",
+        title: "Bad book",
+    },
+    {
+        isbn: "0-1513-9241-0",
+        title: "Terrible book",
+    },
+];
